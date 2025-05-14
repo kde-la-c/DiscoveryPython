bc253ef0b0de1c4a4d582db29caa50bf2a5b6cd3
Kevin De La Colina Kalinowski <kevde-la@c1r3s3.42madrid.com>
2025-05-14 12:12:14 +0200

    fixed module 3

--- module3/ex01/multiplication_table.py
+++ module3/ex01/multiplication_table.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
 
-def get_int(msg, err):
-	while True:
-		try:
-			var = int(input(msg))
-			if var < 0:
-				raise
-			break
-		except:
-			print(err)
-	return var
+# def get_int(msg, err):
+# 	while True:
+# 		try:
+# 			var = int(input(msg))
+# 			if var < 0:
+# 				raise
+# 			break
+# 		except:
+# 			print(err)
+# 	return var
 
-i = get_int(
-    "Enter a number\n",
-	"wrong format"
-)
+# i = get_int(
+# 	"Enter a number\n",
+# 	"wrong format"
+# )
 
-for j in range(10):
-    k = j * i
-    print(f"{j} x {i} = {k}")
-	
+# for j in range(10):
+# 	k = j * i
+# 	print(f"{j} x {i} = {k}")
+
+
+try:
+	i = int(input("Enter a number\n"))
+	for j in range(10):
+		k = j * i
+		print(f"{j} x {i} = {k}")
+except:
+	print("invalid format, enter an integer")
