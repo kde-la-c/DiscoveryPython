40022c1b549843cceec88b8518e0db2eb7e3beb1
kde-la-c <kdelacolinaplant@gmail.com>
2025-05-14 12:41:30 +0200

    fixed calculator

--- module4/ex02/calculator.py
+++ module4/ex02/calculator.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
 
-def get_int(msg):
-	while True:
-		try:
-			var = int(input(msg))
-			break
-		except:
-			print("invalid format, enter an integer")
-	return var
-
-first = get_int("Give me the first number: ")
-second = get_int("Give me the second number: ")
-print("Thank you!")
-print(f"{first} + {second} = {first+second}")
-print(f"{first} - {second} = {first-second}")
-print(f"{first} / {second} = {first/second}")
-print(f"{first} * {second} = {first*second}")
+try:
+	first = int(input("Give me the first number: "))
+	second = int(input("Give me the second number: "))
+	print("Thank you!")
+	print(f"{first} + {second} = {first+second}")
+	print(f"{first} - {second} = {first-second}")
+	try:
+		print(f"{first} / {second} = {first/second}")
+	except:
+		print(f"{first} / {second} = invalid division")
+	print(f"{first} * {second} = {first*second}")
+except:
+	print("invalid format, enter an integer")
