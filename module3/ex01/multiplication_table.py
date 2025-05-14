25cfb5d9cf82dfdfb7487f403724d4fe717a41ce
kde-la-c <kdelacolinaplant@gmail.com>
2025-05-14 12:37:19 +0200

    removed com

--- module3/ex01/multiplication_table.py
+++ module3/ex01/multiplication_table.py
@@ -1,26 +1,5 @@
 #!/usr/bin/env python3
 
-# def get_int(msg, err):
-# 	while True:
-# 		try:
-# 			var = int(input(msg))
-# 			if var < 0:
-# 				raise
-# 			break
-# 		except:
-# 			print(err)
-# 	return var
-
-# i = get_int(
-# 	"Enter a number\n",
-# 	"wrong format"
-# )
-
-# for j in range(10):
-# 	k = j * i
-# 	print(f"{j} x {i} = {k}")
-
-
 try:
 	i = int(input("Enter a number\n"))
 	for j in range(10):
