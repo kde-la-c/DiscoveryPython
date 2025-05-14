a3af0c0895b4dd527231f579ef4bd224de864881
kde-la-c <kdelacolinaplant@gmail.com>
2025-05-14 12:21:14 +0200

    small changes

--- module4/ex01/age.py
+++ module4/ex01/age.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
 
-def get_int(msg):
-	while True:
-		try:
-			var = int(input(msg))
-			if var < 0:
-				raise
-			break
-		except:
-			print("invalid number, enter an integer")
-	return var
-
-age = get_int("Please tell me your age:")
-print(f"You are currently {age} years old.")
-for item in range(3):
-	print(f"in {item*10+10} years, you'll be {age+(item*10)+10} years old")
+try:
+	age = int(input("Please tell me your age:"))
+	print(f"You are currently {age} years old.")
+	for item in range(3):
+		print(f"in {item*10+10} years, you'll be {age+(item*10)+10} years old")
+except:
+	print("invalid number, enter an integer")
